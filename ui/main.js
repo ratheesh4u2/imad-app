0b01f9a00bb94796f583ffe93424c4904ddf1df6
ratheesh4u2 <33595398+ratheesh4u2@users.noreply.github.com>
2018-02-20 17:12:20 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('This is main.js');
 // changing the inner value of main text inside index.html
 var element =document.getElementById("main-text");
 element.innerHTML="Main text is changed by main.js";
+var img =document.getElementByID("madi");
+img.onlick=function()
+{
+    img.style.marginLeft = '200px';
+};
+    
