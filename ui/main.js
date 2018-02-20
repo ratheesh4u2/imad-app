7cc33e7a03d8e2786652e7e4b41479c4ee6ed556
ratheesh4u2 <33595398+ratheesh4u2@users.noreply.github.com>
2018-02-20 23:16:44 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,21 @@ img.onclick=function()
    
 };*/
     
+    var counter=0;
+    
 var button=document.getElementById("counter");
 button.onclick=function()
 {
+    //make a request to the counter endpoint
+    
+    
+    
+    //store the response in a variable
+    
+    
+    //render the variable in the correct span
+    counter=counter+1;
+    var span=getElementById("count");
+    span.innerHTML=counter.toString();
     
 }
