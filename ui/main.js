218cc2bbb0be508bdd36c2118a48c0a997038f34
ratheesh4u2 <33595398+ratheesh4u2@users.noreply.github.com>
2018-02-20 23:27:03 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,7 +33,7 @@ button.onclick=function()
     
     //render the variable in the correct span
     counter=counter+1;
-    var span=getElementById("count");
+    var span=document.getElementById("count");
     span.innerHTML=counter.toString();
     
 }
