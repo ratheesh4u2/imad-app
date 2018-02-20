cec4d473b355944a72ed6ef968c73bd0f1170084
ratheesh4u2 <33595398+ratheesh4u2@users.noreply.github.com>
2018-02-20 17:02:48 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('This is main.js');
 // changing the inner value of main text inside index.html
-var element =document.getelementbyid("main-text");
+var element =document.getElementById("main-text");
 element.innerHTML="Main text is changed by main.js";
