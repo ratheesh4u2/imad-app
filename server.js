7ab71d801d5c871c271c0517222b00ae25734cbe
ratheesh4u2 <33595398+ratheesh4u2@users.noreply.github.com>
2018-03-02 21:28:25 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,25 @@ var config={
 var app = express();
 app.use(morgan('combined'));
 
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    //make as select request
+    //return a response
+    pool.query('SELECT * FROM test',function(err, result){
+       if (err){
+           res.status(500).send(err.toString());
+       } 
+       else {
+           res.send(JSON.stringify(result));
+       }
+       
+       
+    });
+    
+
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -28,11 +47,6 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.ger('/test-db',function(req,res){
-    //make as select request
-    //return a response
-    res.send()
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
